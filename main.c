16b713815c903e765796835775b4706c3c6051ab
Robert Williamson <robert11122002@gmail.com>
2024-01-24 15:26:23 +0000

    sample in c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "xoshiro256.h"
+#include "split.h"
+
+int main(void) {
+  struct Xoshiro256 rng = { 0 };
+  seedSplit(64);
+  seedXoshiro256(&rng, nextSplit());
+
+  for (int i = 0; i < 20; ++i) {
+    // die
+    uint64_t num = nextXoshiro256(&rng) % 6 + 1;
+    printf("%ld\n", num);
+  }
+}
